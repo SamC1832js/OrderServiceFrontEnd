25cf88eb6b36e48fb9a9448fcd5800ce8b4dd9ea
sam156123328 <sam18328578207@gmail.com>
2025-02-09 13:54:15 -0600

    Remove global window interface from environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
-declare global {
-  interface Window {
-    _env_: {
-      API_URL: string;
-    };
-  }
-}
-
 export const environment = {
   production: true,
   apiBaseUrl: 'http://18.191.159.148:8080/api/',
